c39099de5c116d73d6d7b499a600ffe74095e1f1
Heshan <heshan.ua@gmail.com>
2022-10-30 23:00:08 +0530

    Initial lua setup and structure with packer

---
+++ init.lua
@@ -0,0 +1,8 @@
+require('plugins')
+require('plugin-configs.comment')
+
+require('configs.options')
+require('configs.keymaps')
+
+vim.cmd.colorscheme('nightfly');
+
---
+++ lua/configs/keymaps.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader = " "
+
+-- vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true })
+
+vim.keymap.set('n', '<leader>nh', ':nohl<CR>')
+
+vim.keymap.set('n', 'x', '"_x') -- deleting characters using 'x' will not copy to clipboard
+
